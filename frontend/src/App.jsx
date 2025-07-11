5289011f323c8032d5c67d1c74b3c006cc7e0f16
skote05 <144415783+skote05@users.noreply.github.com>
2025-07-12 02:18:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Box, Button } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
+import NavBar from './components/NavBar';
 import {Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
@@ -7,7 +7,7 @@ import CreatePage from './pages/CreatePage';
 function App() {
   return (
     <Box minH={"100vh"}>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path = '/' element={<HomePage/>}/>
         <Route path = '/create' element={<CreatePage/>}/>
